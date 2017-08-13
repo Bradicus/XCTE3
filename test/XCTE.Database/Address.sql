fb8775f666d8d9bed5a1ad3d2ce30f2757e44b14
Brad Ottoson <brado.bmail@gmail.com>
2017-08-13 00:00:35 -0600

    no message

--- test/XCTE.Database/Address.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE [Address] (
-    [id] int, 
-    [street1] VARCHAR(50), 
-    [street2] VARCHAR(50), 
-    [city] VARCHAR(50), 
-    [state] VARCHAR(2), 
-    [zip code] VARCHAR(20)
-    , PRIMARY KEY (id)
-) 
