5258d81503f82d2dc7f8f20ed2069b1bb413c121
Brad Ottoson <brado.bmail@gmail.com>
2017-09-02 17:44:32 -0600

    Added project plan class

---
+++ lib/project_plan.rb
@@ -0,0 +1,39 @@
+
+require 'singleton'
+
+class ProjectPlan
+    attr_accessor :classPlans, :models
+
+    include Singleton
+
+    def initialize
+      @classPlans = Hash.new
+      @models = Hash.new
+    end
+
+    def findClassPlan(unformattedName)
+      for cp in @classPlans
+        
+      end
+    end
+    
+    def findModel(unformattedName)
+      for model in @models
+        if model.name == unformattedName
+          return model
+        end
+      end
+
+      return nil
+    end
+
+    def findClassFunction(languageName, modelName, classPlugName, funPlugName)
+      for model in @models[languageName]
+        if model.name == modelName
+          return model.findClassFunction(classPlugName, funPlugName)
+        end
+      end
+
+      return nil
+    end
+end
